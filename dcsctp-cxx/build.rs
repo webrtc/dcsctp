0a1277ff64e73d5d727866cc8c1b2d36c771d4ae
Emil Lundmark <lndmrk@webrtc.org>
2025-12-19 15:54:22 +0100

    Restructure CXX example code
    The example now live in a directory that explains what it does.
    
    The include/dcsctp.h src/lib.cpp files have been removed since they
    wasn't strictly needed and only made the build pipeline more
    complicated.
    
    The Makefile is now more idiomatic and better show how each file is
    generated and what it depends on. Also, the run step was removed since
    that's not traditionally part of a Makefile.

--- dcsctp-cxx/build.rs
+++ dcsctp-cxx/build.rs
@@ -13,9 +13,7 @@
 // limitations under the License.
 
 fn main() {
-    cxx_build::bridge("src/lib.rs").file("src/lib.cpp").std("c++14").compile("dcsctp-cxx");
+    cxx_build::bridge("src/lib.rs").std("c++14").compile("dcsctp-cxx");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
-    println!("cargo:rerun-if-changed=src/lib.cpp");
-    println!("cargo:rerun-if-changed=include/dcsctp.h");
 }
