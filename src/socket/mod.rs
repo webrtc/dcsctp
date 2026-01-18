823705f583ce001b10d4c8561dcd6f1663b04c47
Victor Boivie <boivie@webrtc.org>
2026-01-18 21:19:37 +0100

    Move stream reset logic to separate module

--- src/socket/mod.rs
+++ src/socket/mod.rs
@@ -44,13 +44,7 @@ use crate::packet::error_chunk::ErrorChunk;
 use crate::packet::forward_tsn_chunk::ForwardTsnChunk;
 use crate::packet::idata_chunk::IDataChunk;
 use crate::packet::iforward_tsn_chunk::IForwardTsnChunk;
-use crate::packet::incoming_ssn_reset_request_parameter::IncomingSsnResetRequestParameter;
 use crate::packet::no_user_data_error_cause::NoUserDataErrorCause;
-use crate::packet::outgoing_ssn_reset_request_parameter::OutgoingSsnResetRequestParameter;
-use crate::packet::parameter::Parameter;
-use crate::packet::re_config_chunk::ReConfigChunk;
-use crate::packet::reconfiguration_response_parameter::ReconfigurationResponseParameter;
-use crate::packet::reconfiguration_response_parameter::ReconfigurationResponseResult;
 use crate::packet::sack_chunk::SackChunk;
 use crate::packet::sctp_packet;
 use crate::packet::sctp_packet::SctpPacket;
@@ -80,8 +74,9 @@ use crate::socket::shutdown::maybe_send_shutdown_ack;
 use crate::socket::shutdown::maybe_send_shutdown_on_packet_received;
 use crate::socket::state::ShutdownSentState;
 use crate::socket::state::State;
-use crate::socket::transmission_control_block::CurrentResetRequest;
-use crate::socket::transmission_control_block::InflightResetRequest;
+use crate::socket::stream_reset::do_reset_streams;
+use crate::socket::stream_reset::handle_reconfig;
+use crate::socket::stream_reset::handle_reconfig_timeout;
 use crate::socket::transmission_control_block::TransmissionControlBlock;
 use crate::timer::BackoffAlgorithm;
 use crate::timer::Timer;
@@ -107,6 +102,7 @@ pub mod heartbeat;
 pub mod shutdown;
 pub mod state;
 pub mod state_cookie;
+pub mod stream_reset;
 pub mod transmission_control_block;
 
 #[cfg(test)]
@@ -394,66 +390,6 @@ impl Socket {
         self.state.tcb().map_or(0, |tcb| tcb.my_verification_tag)
     }
 
-    fn handle_reconfig_timeout(&mut self, now: SocketTime) {
-        let tcb = self.state.tcb_mut().unwrap();
-        if tcb.reconfig_timer.expire(now) {
-            match tcb.current_reset_request {
-                CurrentResetRequest::None => unreachable!(),
-                CurrentResetRequest::Prepared(..) => {
-                    // There is no outstanding request, but there is a prepared one. This means that
-                    // the receiver has previously responded "in progress", which resulted in
-                    // retrying the request (but with a new req_seq_nbr) after a while.
-                }
-                CurrentResetRequest::Inflight(..) => {
-                    // There is an outstanding request, which timed out while waiting for a
-                    // response.
-                    self.ctx.tx_error_counter.increment();
-                    if self.ctx.tx_error_counter.is_exhausted() {
-                        return;
-                    }
-                }
-            }
-            tcb.reconfig_timer.set_duration(tcb.rto.rto());
-            let mut builder = tcb.new_packet();
-            tcb.add_prepared_ssn_reset_request(&mut builder);
-            self.ctx.events.borrow_mut().add(SocketEvent::SendPacket(builder.build()));
-            self.ctx.tx_packets_count += 1;
-        }
-    }
-
-    fn validate_req_seq_nbr(
-        req_seq_nbr: u32,
-        last_processed_req_seq_nbr: u32,
-        last_processed_req_result: ReconfigurationResponseResult,
-        responses: &mut Vec<Parameter>,
-    ) -> bool {
-        if req_seq_nbr == last_processed_req_seq_nbr {
-            // From <https://datatracker.ietf.org/doc/html/rfc6525#section-5.2.1>:
-            //
-            //   If the received RE-CONFIG chunk contains at least one request and based on the
-            //   analysis of the Re-configuration Request Sequence Numbers this is the last received
-            //   RE-CONFIG chunk (i.e., a retransmission), the same RE-CONFIG chunk MUST to be sent
-            //   back in response, as it was earlier.
-            responses.push(Parameter::ReconfigurationResponse(ReconfigurationResponseParameter {
-                response_seq_nbr: req_seq_nbr,
-                result: last_processed_req_result,
-                sender_next_tsn: None,
-                receiver_next_tsn: None,
-            }));
-            return false;
-        } else if req_seq_nbr != last_processed_req_seq_nbr.wrapping_add(1) {
-            // Too old, too new, from wrong association etc.
-            responses.push(Parameter::ReconfigurationResponse(ReconfigurationResponseParameter {
-                response_seq_nbr: req_seq_nbr,
-                result: ReconfigurationResponseResult::ErrorBadSequenceNumber,
-                sender_next_tsn: None,
-                receiver_next_tsn: None,
-            }));
-            return false;
-        }
-        true
-    }
-
     fn handle_forward_tsn(
         &mut self,
         now: SocketTime,
@@ -502,156 +438,6 @@ impl Socket {
             }
         }
         continue_processing
-    }
-
-    fn handle_reconfig(&mut self, now: SocketTime, chunk: ReConfigChunk) {
-        let Some(tcb) = self.state.tcb_mut() else {
-            return;
-        };
-        let mut responses: Vec<Parameter> = Vec::new();
-        for parameter in chunk.parameters {
-            match parameter {
-                Parameter::OutgoingSsnResetRequest(OutgoingSsnResetRequestParameter {
-                    request_seq_nbr,
-                    sender_last_assigned_tsn,
-                    streams,
-                    ..
-                }) => {
-                    if Self::validate_req_seq_nbr(
-                        request_seq_nbr,
-                        tcb.last_processed_req_seq_nbr,
-                        tcb.last_processed_req_result,
-                        &mut responses,
-                    ) {
-                        tcb.last_processed_req_seq_nbr = request_seq_nbr;
-                        tcb.last_processed_req_result = if sender_last_assigned_tsn
-                            > tcb.data_tracker.last_cumulative_acked_tsn()
-                        {
-                            // From <https://datatracker.ietf.org/doc/html/rfc6525#section-5.2.2>:
-                            //
-                            //   E2: If the Sender's Last Assigned TSN is greater than the
-                            //   cumulative acknowledgment point, then the endpoint MUST enter
-                            //   "deferred reset processing".
-                            //
-                            //   [...] If the endpoint enters "deferred reset processing", it MUST
-                            //   put a Re-configuration Response Parameter into a RE-CONFIG chunk
-                            //   indicating "In progress" and MUST send the RE-CONFIG chunk.
-                            tcb.reassembly_queue
-                                .enter_deferred_reset(sender_last_assigned_tsn, &streams);
-                            ReconfigurationResponseResult::InProgress
-                        } else {
-                            // From <https://datatracker.ietf.org/doc/html/rfc6525#section-5.2.2>:
-                            //
-                            //   E3: If no stream numbers are listed in the parameter, then all
-                            //   incoming streams MUST be reset to 0 as the next expected SSN. If
-                            //   specific stream numbers are listed, then only these specific
-                            //   streams MUST be reset to 0, and all other non-listed SSNs remain
-                            //   unchanged.
-                            //
-                            //   E4: Any queued TSNs (queued at step E2) MUST now be released and
-                            //   processed normally."
-                            tcb.reassembly_queue.reset_streams_and_leave_deferred_reset(&streams);
-                            self.ctx
-                                .events
-                                .borrow_mut()
-                                .add(SocketEvent::OnIncomingStreamReset(streams));
-                            ReconfigurationResponseResult::SuccessPerformed
-                        };
-                        responses.push(Parameter::ReconfigurationResponse(
-                            ReconfigurationResponseParameter {
-                                response_seq_nbr: request_seq_nbr,
-                                result: tcb.last_processed_req_result,
-                                sender_next_tsn: None,
-                                receiver_next_tsn: None,
-                            },
-                        ));
-                    }
-                }
-                Parameter::IncomingSsnResetRequest(IncomingSsnResetRequestParameter {
-                    request_seq_nbr,
-                    ..
-                }) => {
-                    if Self::validate_req_seq_nbr(
-                        request_seq_nbr,
-                        tcb.last_processed_req_seq_nbr,
-                        tcb.last_processed_req_result,
-                        &mut responses,
-                    ) {
-                        responses.push(Parameter::ReconfigurationResponse(
-                            ReconfigurationResponseParameter {
-                                response_seq_nbr: request_seq_nbr,
-                                result: ReconfigurationResponseResult::SuccessNothingToDo,
-                                sender_next_tsn: None,
-                                receiver_next_tsn: None,
-                            },
-                        ));
-                        tcb.last_processed_req_seq_nbr = request_seq_nbr;
-                    }
-                }
-                Parameter::ReconfigurationResponse(ReconfigurationResponseParameter {
-                    response_seq_nbr,
-                    result,
-                    ..
-                }) => {
-                    if let CurrentResetRequest::Inflight(InflightResetRequest {
-                        request_sequence_number,
-                        request,
-                    }) = &tcb.current_reset_request
-                    {
-                        if response_seq_nbr == *request_sequence_number {
-                            tcb.reconfig_timer.stop();
-
-                            tcb.current_reset_request = match result {
-                                ReconfigurationResponseResult::SuccessNothingToDo
-                                | ReconfigurationResponseResult::SuccessPerformed => {
-                                    self.ctx.events.borrow_mut().add(
-                                        SocketEvent::OnStreamsResetPerformed(
-                                            request.streams.clone(),
-                                        ),
-                                    );
-                                    self.ctx.send_queue.commit_reset_streams();
-
-                                    CurrentResetRequest::None
-                                }
-                                ReconfigurationResponseResult::InProgress => {
-                                    tcb.reconfig_timer.set_duration(tcb.rto.rto());
-                                    tcb.reconfig_timer.start(now);
-
-                                    CurrentResetRequest::Prepared(request.clone())
-                                }
-                                ReconfigurationResponseResult::Denied
-                                | ReconfigurationResponseResult::ErrorWrongSSN
-                                | ReconfigurationResponseResult::ErrorRequestAlreadyInProgress
-                                | ReconfigurationResponseResult::ErrorBadSequenceNumber => {
-                                    self.ctx.events.borrow_mut().add(
-                                        SocketEvent::OnStreamsResetFailed(request.streams.clone()),
-                                    );
-                                    self.ctx.send_queue.rollback_reset_streams();
-
-                                    CurrentResetRequest::None
-                                }
-                            }
-                        }
-                    }
-                }
-                _ => {}
-            }
-        }
-
-        if !responses.is_empty() {
-            self.ctx.events.borrow_mut().add(SocketEvent::SendPacket(
-                tcb.new_packet()
-                    .add(&Chunk::ReConfig(ReConfigChunk { parameters: responses }))
-                    .build(),
-            ));
-            self.ctx.tx_packets_count += 1;
-        }
-
-        // Note: Handling this response may result in outgoing stream resets finishing (either
-        // successfully or with failure). If there still are pending streams that were waiting for
-        // this request to finish, continue resetting them. Also, if a response was processed,
-        // pending to-be-reset streams may now have become unpaused. Try to send more DATA chunks.
-        self.ctx.send_buffered_packets(&mut self.state, now);
     }
 
     fn validate_send(&self, message: &Message, send_options: &SendOptions) -> SendStatus {
@@ -773,7 +559,9 @@ impl DcSctpSocket for Socket {
                         Chunk::ShutdownComplete(c) => {
                             handle_shutdown_complete(&mut self.state, &mut self.ctx, c);
                         }
-                        Chunk::ReConfig(c) => self.handle_reconfig(now, c),
+                        Chunk::ReConfig(c) => {
+                            handle_reconfig(&mut self.state, &mut self.ctx, now, c);
+                        }
                         Chunk::ForwardTsn(ForwardTsnChunk {
                             new_cumulative_tsn,
                             skipped_streams,
@@ -822,7 +610,7 @@ impl DcSctpSocket for Socket {
                     self.ctx.tx_error_counter.increment();
                 }
                 handle_heartbeat_timeouts(&mut self.state, &mut self.ctx, now);
-                self.handle_reconfig_timeout(now);
+                handle_reconfig_timeout(&mut self.state, &mut self.ctx, now);
                 handle_t2_shutdown_timeout(&mut self.state, &mut self.ctx, now);
             }
         }
@@ -980,21 +768,8 @@ impl DcSctpSocket for Socket {
     }
 
     fn reset_streams(&mut self, outgoing_streams: &[StreamId]) -> ResetStreamsStatus {
-        let Some(tcb) = self.state.tcb_mut() else {
-            return ResetStreamsStatus::NotConnected;
-        };
-        if !tcb.capabilities.reconfig {
-            return ResetStreamsStatus::NotSupported;
-        }
         let now = *self.now.borrow();
-        for stream_id in outgoing_streams {
-            self.ctx.send_queue.prepare_reset_stream(*stream_id);
-        }
-
-        // This will send the SSN reset request control messagae.
-        self.ctx.send_buffered_packets(&mut self.state, now);
-
-        ResetStreamsStatus::Performed
+        do_reset_streams(&mut self.state, &mut self.ctx, now, outgoing_streams)
     }
 
     fn buffered_amount(&self, stream_id: StreamId) -> usize {
