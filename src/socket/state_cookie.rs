805cf731e6ad90684db75ee13ed2d6667909afe7
Victor Boivie <boivie@webrtc.org>
2026-07-08 12:05:45 +0200

    Limit the effect of state cookie tampering
    This commit enhances the security and robustness of the state cookie by
    limiting what an attacker can achieve even if they manage to forge or 
    tamper with the cookie. Note that we don't add any signature or digest 
    right now, as that would be done by adding library dependencies that the
    downstream projects don't use right now (sha2 and hmac). This might be
    rivisited in the future.
    
    By storing the raw peer capabilities in the state cookie, we prevent an 
    attacker from enabling extensions that the local socket didn't signal
    support for, limit the stream limits (incoming and outgoing) from what
    was originally announced, and also elimintate some duplication in 
    capability parsing in the different methods to do connection handshakes.
    
    This defense-in-depth measure ensures that the state instantiated from
    a COOKIE ECHO strictly matches the parameters of the original INIT, and
    if an attacker tampers with it, the effects are very limited.

--- src/socket/state_cookie.rs
+++ src/socket/state_cookie.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use crate::api::ZeroChecksumAlternateErrorDetectionMethod;
 use crate::packet::read_u16_be;
 use crate::packet::read_u32_be;
 use crate::packet::read_u64_be;
@@ -21,7 +22,7 @@ use crate::packet::write_u64_be;
 use crate::socket::capabilities::Capabilities;
 use crate::types::Tsn;
 
-const COOKIE_SIZE: usize = 44;
+const COOKIE_SIZE: usize = 48;
 const MAGIC_1: u32 = 1684230979;
 const MAGIC_2: u32 = 1414558256;
 
@@ -32,7 +33,7 @@ pub struct StateCookie {
     pub my_initial_tsn: Tsn,
     pub a_rwnd: u32,
     pub tie_tag: u64,
-    pub capabilities: Capabilities,
+    pub peer_capabilities: Capabilities,
 }
 
 impl StateCookie {
@@ -47,20 +48,28 @@ impl StateCookie {
             return Err("Invalid state cookie magic");
         }
 
+        let peer_tag = read_u32_be!(&data[8..12]);
+        let my_tag = read_u32_be!(&data[12..16]);
+        if peer_tag == 0 || my_tag == 0 {
+            return Err("Invalid verification tag in state cookie");
+        }
+
         Ok(StateCookie {
-            peer_tag: read_u32_be!(&data[8..12]),
-            my_tag: read_u32_be!(&data[12..16]),
+            peer_tag,
+            my_tag,
             peer_initial_tsn: Tsn(read_u32_be!(&data[16..20])),
             my_initial_tsn: Tsn(read_u32_be!(&data[20..24])),
             a_rwnd: read_u32_be!(&data[24..28]),
             tie_tag: read_u64_be!(&data[28..36]),
-            capabilities: Capabilities {
+            peer_capabilities: Capabilities {
                 partial_reliability: data[36] != 0,
                 message_interleaving: data[37] != 0,
                 reconfig: data[38] != 0,
-                zero_checksum: data[39] != 0,
-                negotiated_maximum_incoming_streams: read_u16_be!(&data[40..42]),
-                negotiated_maximum_outgoing_streams: read_u16_be!(&data[42..44]),
+                zero_checksum_method: ZeroChecksumAlternateErrorDetectionMethod(read_u32_be!(
+                    &data[40..44]
+                )),
+                negotiated_maximum_incoming_streams: read_u16_be!(&data[44..46]),
+                negotiated_maximum_outgoing_streams: read_u16_be!(&data[46..48]),
             },
         })
     }
@@ -75,12 +84,20 @@ impl StateCookie {
         write_u32_be!(&mut data[20..24], self.my_initial_tsn.0);
         write_u32_be!(&mut data[24..28], self.a_rwnd);
         write_u64_be!(&mut data[28..36], self.tie_tag);
-        data[36] = self.capabilities.partial_reliability as u8;
-        data[37] = self.capabilities.message_interleaving as u8;
-        data[38] = self.capabilities.reconfig as u8;
-        data[39] = self.capabilities.zero_checksum as u8;
-        write_u16_be!(&mut data[40..42], self.capabilities.negotiated_maximum_incoming_streams);
-        write_u16_be!(&mut data[42..44], self.capabilities.negotiated_maximum_outgoing_streams);
+        data[36] = self.peer_capabilities.partial_reliability as u8;
+        data[37] = self.peer_capabilities.message_interleaving as u8;
+        data[38] = self.peer_capabilities.reconfig as u8;
+        data[39] = 0; // padding
+        write_u32_be!(&mut data[40..44], self.peer_capabilities.zero_checksum_method.0);
+        write_u16_be!(
+            &mut data[44..46],
+            self.peer_capabilities.negotiated_maximum_incoming_streams
+        );
+        write_u16_be!(
+            &mut data[46..48],
+            self.peer_capabilities.negotiated_maximum_outgoing_streams
+        );
+
         data
     }
 }
@@ -98,11 +115,11 @@ mod tests {
             my_initial_tsn: Tsn(654),
             a_rwnd: 789,
             tie_tag: 1020304050,
-            capabilities: Capabilities {
+            peer_capabilities: Capabilities {
                 partial_reliability: true,
                 message_interleaving: false,
                 reconfig: true,
-                zero_checksum: true,
+                zero_checksum_method: ZeroChecksumAlternateErrorDetectionMethod(1),
                 negotiated_maximum_incoming_streams: 123,
                 negotiated_maximum_outgoing_streams: 234,
             },
@@ -117,11 +134,14 @@ mod tests {
         assert_eq!(deserialized.my_initial_tsn, Tsn(654));
         assert_eq!(deserialized.a_rwnd, 789);
         assert_eq!(deserialized.tie_tag, 1020304050);
-        assert!(deserialized.capabilities.partial_reliability);
-        assert!(!deserialized.capabilities.message_interleaving);
-        assert!(deserialized.capabilities.reconfig);
-        assert!(deserialized.capabilities.zero_checksum);
-        assert_eq!(deserialized.capabilities.negotiated_maximum_incoming_streams, 123);
-        assert_eq!(deserialized.capabilities.negotiated_maximum_outgoing_streams, 234);
+        assert!(deserialized.peer_capabilities.partial_reliability);
+        assert!(!deserialized.peer_capabilities.message_interleaving);
+        assert!(deserialized.peer_capabilities.reconfig);
+        assert_eq!(
+            deserialized.peer_capabilities.zero_checksum_method,
+            ZeroChecksumAlternateErrorDetectionMethod(1)
+        );
+        assert_eq!(deserialized.peer_capabilities.negotiated_maximum_incoming_streams, 123);
+        assert_eq!(deserialized.peer_capabilities.negotiated_maximum_outgoing_streams, 234);
     }
 }
